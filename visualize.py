2fa6f3f7a5a2bb06b4c7329339f7c6d1d9cb29e9
limyunkai19 <yunkai96@hotmail.com>
2018-01-31 21:12:17 +0800

    Implement Visualize class and bug fix

--- visualize.py
+++ visualize.py
@@ -13,6 +13,15 @@ from torch.autograd import Variable
 import utils
 
 class GradCAM:
+    """
+    Gradient-weighted Class Activation Mapping (Grad-CAM)
+    Get a coarse heatmap of activation highlighting important location in
+    input image based on the gradient of (last) convolutional layer to
+    achieve "visual explanation" for CNN prediction
+
+    Reference: https://arxiv.org/abs/1610.02391
+    """
+
     def __init__(self, model, transform, target_layer, cuda=False):
         self.model = model
         self.model.train(False)
@@ -39,37 +48,41 @@ class GradCAM:
         """ The forward pass
 
         Argument:
-            img (Tensor) - the (unprocessed) input image
+            img (PIL Image) - the (unprocessed) input image
 
         Return:
-            Tensor/Dict
+            Tensor/Dict - the output of the model
         """
 
         # preprocess the PIL image first
-        img_tensor = preprocess(img)
+        img_tensor = self.transform(img)
         img_tensor.unsqueeze_(0) # this add a dimension as a dummy "batch"
         img_variable = Variable(img_tensor)
         self.output = self.model(img_variable)
         return self.output.data
 
+    def backward(self, idx, sorted_idx=False):
+        self.model.zero_grad()
+        self.output.backward(gradient=utils.one_hot_tensor(idx, 1000), retain_graph=True)
 
-    def backward(self, idx, sorted=False):
+    def get_gradcam_intensity(self, idx, sorted_idx=False, backward=True):
         """ The (partial) backward pass and generate GradCAM intensity value for each pixel
 
         Argument:
             idx (int) - the idx of the class to be localize by GradCAM
-            sorted (bool) - if sorted==True, the idx[0] will be the class with highest score,
+            sorted_idx (bool) - if sorted_idx==True, the idx[0] will be the class with highest score,
                 idx[1] will be the class with second highest score and so on
+            backward (bool) - perform backward pass or not,
+                                under normal usecase, it should be true
 
         Return:
             Tensor (size == kernal size of target layer)
                 - GradCAM intensity value for each pixel
         """
 
-        self.model.zero_grad()
-        #implement sorted !!!
-        self.output.backward(gradient=utils.one_hot_tensor(idx, 1000), retain_graph=True)
-
+        # implement sorted_idx !!!
+        if backward:
+            self.backward(idx)
         # self.feature_maps # 1x2048x7x7
         # self.gradients # 1x2048x7x7
 
@@ -120,10 +133,9 @@ class GradCAM:
 
         return Image.fromarray(np.uint8((heat_map[:,:,:3]+original_img)/2.0)), intensity
 
-
 class Backpropagation:
     """
-    Vanilla Backpropagation.
+    Vanilla Backpropagation
     Backpropagate to input then get the gradient at input
     """
 
@@ -140,13 +152,13 @@ class Backpropagation:
         """ The forward pass
 
         Argument:
-            img (Tensor) - the (unprocessed) input image
+            img (PIL Image) - the (unprocessed) input image
 
         Return:
-            Tensor/Dict
+            Tensor/Dict - the output of the model
         """
 
-        img_tensor = preprocess(img)
+        img_tensor = self.transform(img)
         img_tensor.unsqueeze_(0) # this add a dimension as a dummy "batch"
         img_variable = Variable(img_tensor, requires_grad=True)
 
@@ -154,30 +166,36 @@ class Backpropagation:
         self.output = self.model(img_variable)
         return self.output.data
 
+    def backward(self, idx):
+        self.model.zero_grad()
+        self.output.backward(gradient=utils.one_hot_tensor(idx, 1000), retain_graph=True)
 
-    def backward(self, idx, sorted=False):
+    def get_input_gradient(self, idx, sorted_idx=False, backward=True):
         """ The backward pass and return the gradient at input image
 
         Argument:
             idx (int) - the idx of the class to be localize by GradCAM
-            sorted (bool) - if sorted==True, the idx[0] will be the class with highest score,
+            sorted_idx (bool) - if sorted_idx==True, the idx[0] will be the class with highest score,
                 idx[1] will be the class with second highest score and so on
+            backward (bool) - perform backward pass or not,
+                                under normal usecase, it should be true
 
         Return:
-            PIL image - The gradient value for each pixel
+            PIL image - The RGB gradient images generated based on the gradient value
+            Numpy array - The gradient value for each pixel (range: [0-1])
         """
 
-        self.model.zero_grad()
-        #implement sorted !!!
-        self.output.backward(gradient=utils.one_hot_tensor(idx, 1000), retain_graph=True)
+        #implement sorted_idx !!!
 
-        gradient = self.input.grad.data
-        gradient = utils.normalize(gradient)*255
+        if backward:
+            self.backward(idx)
+
+        gradient = utils.normalize(self.input.grad.data)
 
         # 1x3x224x224 -> 224x224x3
         gradient = gradient.cpu().numpy()[0].transpose(1, 2, 0)
 
-        return Image.fromarray(np.uint8(gradient))
+        return Image.fromarray(np.uint8(gradient*255)), gradient
 
 class GuidedBackpropagation(Backpropagation):
     """
@@ -187,10 +205,16 @@ class GuidedBackpropagation(Backpropagation):
     GuidedBackprop: input>0 * gradin>0 * gradin on relu.backward
     but original relu had implemented relu gradin = input>0 * gradin
     thus we only need to add gradin>0 * relu gradin
+
+    Reference: https://arxiv.org/abs/1412.6806
+
+    NOTE:
+        The gradient on back propagation of the model will be modify, if this is
+        not the desired behaeviour, construct this class with a deepcopy of model
     """
 
     def __init__(self, model, transform, cuda=False):
-        super().__init__(model, transforms, cuda)
+        super().__init__(model, transform, cuda)
 
         # define hook function
         def backward_hook(module, grad_input, grad_output):
@@ -203,15 +227,244 @@ class GuidedBackpropagation(Backpropagation):
             if isinstance(module, torch.nn.ReLU):
                 module.register_backward_hook(backward_hook)
 
-# Use Visualize wrapper class for GuidedGradCAM for consistancy
-# class GuidedGradCAM:
-
-# class Visualize:
+class GuidedGradCAM:
     """
-    A warpper class for all the visualization algorithm to keep model consistancy
+    Guided Grad-CAM
+    Use the heatmap of Grad-CAM to produce a localized guided-backprop saliency
+
+    Reference: https://arxiv.org/abs/1610.02391
+
+    NOTE:
+        This class is present for completeness and consistancy.
+        For general visualization usage, please use the Visualize wrapper class
+    """
+    def __init__(self, model, transform, target_layer, cuda=False):
+        self.model = model
+        self.cuda = cuda
+        self.transform = transform
+        self.target_layer = target_layer
+
+        self.GradCAM = GradCAM(model, transform, target_layer, cuda)
+        self.GuidedBackprop = GuidedBackpropagation(copy.deepcopy(model),
+                                                    transform, cuda)
+
+    def forward(self, img):
+        """ The forward pass
+        Argument:
+            img (Tensor) - the (unprocessed) input image
+        Return:
+            Tensor/Dict
+        """
+        pass
+
+class Visualize:
+    """
+    Warpper class of all the visualizatiom for efficiency and consistancy
+
+    NOTE:
+        I know some part of this class looks weird and redundant but my aims
+        is to make different classes share the same model for efficiency
     """
 
+    def __init__(self, model, transform, target_layer, retainModel=True, cuda=False):
+        self.model = model
+        self.cuda = cuda
+        self.transform = transform
+        self.target_layer = target_layer
 
+        if retainModel:
+            self.model = copy.deepcopy(model)
+
+        self.gradCAM = GradCAM(self.model, self.transform,
+                               self.target_layer, self.cuda)
+        self.vBackprop = Backpropagation(self.model, self.transform, self.cuda)
+        self.gBackprop = GuidedBackpropagation(
+            # guided backprop will change the gradient on back prop
+            # need a copy of model
+            copy.deepcopy(model),
+            self.transform,
+            self.cuda
+        )
+
+    def input_image(self, image):
+        """ Supply input image for "visual explanation"
+
+        Argument:
+            img (PIL Image) - the (unprocessed) input image
+
+        Return:
+            None
+        """
+
+        self.image = image
+        img_tensor = self.transform(image)
+        img_tensor.unsqueeze_(0) # this add a dimension as a dummy "batch"
+        img_variable = Variable(img_tensor, requires_grad=True)
+
+        self.input = img_variable
+        self.ginput = copy.deepcopy(img_variable)
+
+        self._forwarded = False
+        self._forwarded_gbackprop = False
+        self._model_idx = None
+        self._gmodel_idx = None
+        self._gradcam_intensity = None
+        self._gradcam_intensity_idx = None
+        self._vbackprop_gradient = None
+        self._vbackprop_gradient_idx = None
+        self._gbackprop_gradient = None
+        self._gbackprop_gradient_idx = None
+
+        # self.output = self.model(img_variable)
+        # return self.output.data
+
+
+    def get_prediction_output(self):
+        """ Get the prediction output of the model
+
+        Argument:
+            None
+
+        Return:
+            Tensor - the prediction output of the model
+        """
+
+        if self._forwarded:
+            return self.output.data
+        if self._forwarded_gbackprop:
+            return self.output_gbackprop.data
+
+        self.output = self.model(self.input)
+        self.gradCAM.input = self.input
+        self.vBackprop.input = self.input
+        self._forwarded = True
+
+        return self.output.data
+
+    def get_gradcam_intensity(self, idx, sorted_idx=False):
+        """ Get the GradCAM intensity value for each pixel
+
+        Argument:
+            idx (int) - the idx of the class to be localize by GradCAM
+            sorted_idx (bool) - if sorted_idx==True, the idx[0] will be the class with highest score,
+                idx[1] will be the class with second highest score and so on
+
+        Return:
+            Tensor (size == kernal size of target layer)
+                - GradCAM intensity value for each pixel
+        """
+
+        if sorted_idx:
+            # implement sorted_idx at here to keep idx consistancy
+            # idx = sorted_idx(idx)
+            pass
+
+        if self._gradcam_intensity_idx == idx:
+            return self._gradcam_intensity
+
+        if not self._forwarded:
+            self.output = self.model(self.input)
+            self.gradCAM.input = self.input
+            self.vBackprop.input = self.input
+            self._forwarded = True
+
+        if self._model_idx != idx:
+            self.model.zero_grad()
+            self.output.backward(gradient=utils.one_hot_tensor(idx, 1000), retain_graph=True)
+            self._model_idx = idx
+
+        self._gradcam_intensity = self.gradCAM.get_gradcam_intensity(idx, backward=False)
+        self._gradcam_intensity_idx = idx;
+
+        return self._gradcam_intensity
+
+    def get_gradcam_heatmap(self, idx, sorted_idx=False):
+        """ Get the GradCAM heatmap on the original input image
+            and intensity value that is same size as original input image
+
+        Argument:
+            idx (int) - the idx of the class to be localize by GradCAM
+            sorted_idx (bool) - if sorted_idx==True, the idx[0] will be the class with highest score,
+                idx[1] will be the class with second highest score and so on
+
+        Return:
+            PIL image - The original image with GradCAM intensity applied to
+            Numpy array - The intensity same size as original image (range: [0-1])
+        """
+
+        intensity = self.get_gradcam_intensity(idx, sorted_idx)
+        return self.gradCAM.apply_color_map(intensity, self.image)
+
+    def get_vanilla_backprop_gradient(self, idx, sorted_idx=False):
+        """
+
+        Return:
+            PIL image - The gradient image
+            Numpy array - The gradient of original image (range: [0-1])
+        """
+        if sorted_idx:
+            # implement sorted_idx at here to keep idx consistancy
+            # idx = sorted_idx(idx)
+            pass
+
+        if self._vbackprop_gradient_idx == idx:
+            return self._vbackprop_gradient
+
+        if not self._forwarded:
+            self.output = self.model(self.input)
+            self.gradCAM.input = self.input
+            self.vBackprop.input = self.input
+            self._forwarded = True
+
+        if self._model_idx != idx:
+            self.model.zero_grad()
+            self.output.backward(gradient=utils.one_hot_tensor(idx, 1000), retain_graph=True)
+            self._model_idx = idx
+
+        self._vbackprop_gradient = self.vBackprop.get_input_gradient(idx, backward=False)
+        self._vbackprop_gradient_idx = idx;
+
+        return self._vbackprop_gradient
+
+    # def get_vanilla_backprop_saliency(self, idx, sorted_idx=False):
+    #     gradient = self.get_vanilla_backprop_gradient(idx, sorted_idx)
+    #
+    #     return utils.normalize(gradient)
+
+    def get_guided_backprop_gradient(self, idx, sorted_idx=False):
+        if sorted_idx:
+            # implement sorted_idx at here to keep idx consistancy
+            # idx = sorted_idx(idx)
+            pass
+
+        if self._gbackprop_gradient_idx == idx:
+            return self._gbackprop_gradient
+
+        if not self._forwarded_gbackprop:
+            self.output_gbackprop = self.gBackprop.model(self.ginput)
+            self.gBackprop.input = self.ginput
+            self._forwarded_gbackprop = True
+
+        if self._gmodel_idx != idx:
+            self.gBackprop.model.zero_grad()
+            self.output_gbackprop.backward(gradient=utils.one_hot_tensor(idx, 1000), retain_graph=True)
+            self._gmodel_idx = idx
+
+        self._gbackprop_gradient = self.gBackprop.get_input_gradient(idx, backward=False)
+        self._gbackprop_gradient_idx = idx;
+
+        return self._gbackprop_gradient
+
+    # def get_guided_backprop_saliency(self, idx, sorted_idx=False):
+    #     gradient = self.get_guided_backprop_gradient(idx, sorted_idx)
+    #
+    #     return utils.normalize(gradient)
+    #
+    # def get_guided_gramcam_saliency(self, idx, sorted_idx=False):
+    #     gbackprop = self.get_guided_backprop_saliency(idx, sorted)
+    #     gradcam = self.get_gradcam_intensity(idx, sorted)[1]
+    #
+    #     return gBackprop*gradcam
 
 if __name__ == '__main__':
     import sys
@@ -250,14 +503,13 @@ if __name__ == '__main__':
     print("Top 3 prediction")
     for i in range(3):
         idx = score.argmax()
-        intensity = gradcam.backward(idx)
+        intensity = gradcam.get_gradcam_intensity(idx)
         gradcam_img, gradcam_intensity = gradcam.apply_color_map(intensity, img_pil)
 
-        gbackprop_img = guidedbackprop.backward(idx)
+        gbackprop_img, _ = guidedbackprop.get_input_gradient(idx)
         # guidedbackprop.backward(idx1).convert('L').save('gb.png')
 
-        backprop_img = backprop.backward(idx)
-
+        backprop_img, _ = backprop.get_input_gradient(idx)
 
         gbackprop_arr = np.array(gbackprop_img.convert("L"), dtype='float')
         # gbackprop_arr = utils.normalize(gbackprop_arr)
